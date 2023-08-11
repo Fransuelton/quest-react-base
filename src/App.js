a78420f1171ad126be44c90ec76d7191303fe23e
Fransuelton <Elton6103@gmail.com>
2023-08-11 12:06:02 -0300

    i added the react and props elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Paragraph from './components/paragraph/paragraph';
+import Button from './components/buttton/button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Paragraph
+        content='Este é o desafio de React Básico do curso DevQuest.'
+        style={{ color: 'red', textTransform: 'upperCase' }}
+      />
+
+      <Button label='Baixar CV' />
+    </>
   );
 }
 
