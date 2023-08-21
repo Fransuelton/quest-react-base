73355990f294d737ac1527040ab590c88ebfe3da
Fransuelton <Elton6103@gmail.com>
2023-08-21 15:44:31 -0300

    fix: Fixes the import of components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Paragraph from './components/paragraph/paragraph';
-import Button from './components/buttton/button';
+import Paragraph from './components/StyledText/index';
+import Button from './components/AlertButtton/index';
 
 function App() {
   return (
